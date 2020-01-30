f6841a795e1a66343b45015106e5591d7fb239a4
federico <f.fulgeri@gmail.com>
2020-01-30 07:59:29 +0100

    animations 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,19 @@ import { PromotionService } from '../services/promotion.service';
 import { LeaderService } from '../services/leader.service'
 import { Leader } from '../shared/leader';
 import { baseURL } from '../shared/baseurl';
+import { flyInOut } from '../animations/app.animations'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host:{
+    '[@flyInOut]':'true',
+    'style': 'display: block;'
+  },
+  animations:[
+    flyInOut()
+  ]
 })
 export class HomeComponent implements OnInit {
 
